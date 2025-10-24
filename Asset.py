50f3654f6efe0c157a4c89bca91c5024f1d827ac
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-10-24 23:25:36 +1030

    Defining functions for class asset

--- Asset.py
+++ Asset.py
@@ -12,3 +12,23 @@ class Asset:
         self.__name = name
         self.__description = description
         self.__encrypted = False
+
+    def get_name(self):
+        return self.__name
+
+    def is_encrypted(self):
+        return self.__encrypted
+
+    def decrypt(self):
+        if self.__encrypted:
+            self.__encrypted = False
+            print(f"Decrypted {self.__name}")
+        else:
+            print(f"{self.__name} was already decrypted.")
+
+    def encrypt(self):
+        if not self.__encrypted:
+            self.__encrypted = True
+            print(f"Encrypted {self.__name}")
+        else:
+            print(f"{self.__name} was already encrypted.")
