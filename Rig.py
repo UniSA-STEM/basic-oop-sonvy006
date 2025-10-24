9472bdad27b3faee59d8c08f904eb556e1816e54
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-10-24 23:46:13 +1030

    created class Rig and defined get functions.

--- Rig.py
+++ Rig.py
@@ -6,3 +6,30 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from Asset import Asset
+
+class Rig:
+    def __init__(self, name):
+        self.__name = name
+        self.__damage = 0
+        self.__broken = False
+        self.__level = 0
+        self.__storage = [
+            Asset("Data Spike", "Used in battles"),
+            Asset("Data Spike", "Used in battles"),
+            Asset("Removable Drive", "used to extract assets in battles ")
+        ]
+    def get_name(self):
+        return self.__name
+
+    def get_level(self):
+        return self.__level
+
+    def  get_storage(self):
+        return self.__storage
+
+    def is_broken(self):
+        return self.__broken
+
+
+
