1ed4ea5872f7f2b0ef879b62fac0514e882f6329
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-10-25 16:48:08 +1030

    Added import statement to import hacker from hacker file and added main.py simulation with two hackers and print their names.

--- Main.py
+++ Main.py
@@ -6,3 +6,14 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from Hacker import Hacker
+print("---- Welcome to Into the Grid (Proper__str__Version)----\n")
+
+hacker1 = Hacker("Cipher")
+hacker2 = Hacker("ErrorDestroyer")
+
+print(hacker1)
+print(hacker2)
+
+
+
