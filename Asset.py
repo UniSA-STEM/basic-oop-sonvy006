fb83c539bf876fc8be4dfa3913cc6f7cf470a21c
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-10-25 17:15:25 +1030

    Added full docstrings and comments in between of lines to Asset.py for readability and my clarity.

--- Asset.py
+++ Asset.py
@@ -1,25 +1,46 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Creating and defining class asset that shows digital assets used by hackers and rigs.>
+Author: Vishesh Soni
+ID: 1103871387
+Username: sonvy006
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
 class Asset:
+    """
+    The asset class represent items in within the simulation
+    The actions by hackers or rigs can be encrypting or decrypting of assets.
+    """
     def __init__(self, name, description):
+        """
+        Initiator method that creates a new asset object
+        :param name: Asset Name (eg: CryptoToken or Data Spike)
+        :param description: A brief text of the use of the asset.
+        """
         self.__name = name
         self.__description = description
         self.__encrypted = False
 
     def get_name(self):
+        """
+        returns the name of the asset.
+        :return: string name of the asset.
+        """
         return self.__name
 
     def is_encrypted(self):
+        """
+        checks if the asset is encrypted.
+        :return: whether the asset is encrypted or not.
+        """
         return self.__encrypted
 
     def decrypt(self):
+        """
+        Decrypts the asset
+        :return: obtains the encrypted asset and decrypts it.
+        """
         if self.__encrypted:
             self.__encrypted = False
             print(f"Decrypted {self.__name}")
@@ -27,6 +48,10 @@ class Asset:
             print(f"{self.__name} was already decrypted.")
 
     def encrypt(self):
+        """
+        Encrypts the asset.
+        :return: true if encrypted else false.
+        """
         if not self.__encrypted:
             self.__encrypted = True
             print(f"Encrypted {self.__name}")
@@ -34,6 +59,12 @@ class Asset:
             print(f"{self.__name} was already encrypted.")
 
     def __str__(self):
+        """
+        String representation of the asset.
+        breaks down the readable summary of the asset
+        including its name and description.
+        :return:
+        """
         if self.__encrypted:
             return f"{self.__name} was described as '{self.__description}' and is encrypted."
         else:
