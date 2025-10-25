2ca78d64e50466e7d6e4c4c21932952c8b03a3a8
sonvy006 <sonvy006@mymail.unisa.edu.au>
2025-10-25 13:07:15 +1030

    created class Hacker and added get functions

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,24 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from Asset import Asset
+
+class Hacker:
+    def __init__(self, name):
+        self.__name = name
+        self.__inventory = [Asset("CryptoToken", "Used to repair or buy rigs")]
+        self.__rig = None
+        self.__trace = 0
+
+    def get_name(self):
+        return self.__name
+
+    def get_inventory(self):
+        return self.__inventory
+
+    def get_rig(self):
+        return self.__rig
+
+    def get_trace(self):
+        return self.__trace
